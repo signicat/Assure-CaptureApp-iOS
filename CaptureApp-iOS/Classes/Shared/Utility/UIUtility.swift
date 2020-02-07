f073f357eca7cef08b2e234c0c7520b5d55b13a6
Tiago Mendes <Tiago.Mendes@signicat.com>
2020-02-07 10:00:20 +0000

    The load image was falling in the demo project, now we deal with both situations

--- CaptureApp-iOS/Classes/Shared/Utility/UIUtility.swift
+++ CaptureApp-iOS/Classes/Shared/Utility/UIUtility.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class UIUtility {
 
-
+    // Load image in this demo project is different from loading image in a project that implements this pod.
+    // This code deals with both situations.
     public func loadImageFromFile(fileName: String, ofType: String) -> UIImage? {
         
         let bundle = Bundle(for: type(of: self))
-        return UIImage(named: "CaptureApp-iOS.bundle/\(fileName).\(ofType)", in: bundle, compatibleWith: nil)
+        if let image = UIImage(named: "CaptureApp-iOS.bundle/\(fileName).\(ofType)", in: bundle, compatibleWith: nil) {
+            
+            return image
+        }
+        
+        guard let path = bundle.path(forResource: fileName, ofType: ofType) else {
+            return nil
+        }
+        let fileURL = URL(fileURLWithPath: path)
+        do {
+            let imageData = try Data(contentsOf: fileURL)
+            return UIImage(data: imageData)
+        } catch {
+            print("Error loading image : \(error)")
+        }
+        return nil
     }
     
 }
